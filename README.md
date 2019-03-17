056b607f0603b0b966f760028399c90db4d53c53
sjnya <sjnyas@gmail.com>
2019-03-17 17:56:01 +0900

    Use cloud functions

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ npm install
 # serve with hot reload at localhost:8080
 npm run dev
 
+# serve with at localhost:5000
+firebase serve --only functions
+
 # build for production with minification
 npm run build
 
@@ -32,6 +35,10 @@ firebase login
 firebase init hosting
 npm run build
 firebase deploy
+
+# Deploy Functions to Firebase
+firebase functions:config:get
+firebase deploy --only functions
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,69 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+const rp = require("request-promise");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.exchangeToken = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    if (request.method !== "POST") {
+      return response.status(401).json({
+        message: "Not allowed"
+      });
+    }
+    rp({
+      method: "POST",
+      uri: "https://www.googleapis.com/oauth2/v4/token",
+      timeout: 30 * 1000,
+      json: {
+        client_id: functions.config().gapi.client_id,
+        client_secret: functions.config().gapi.client_secret,
+        code: request.body.code,
+        grant_type: "authorization_code",
+        redirect_uri: request.body.redirect_uri
+      }
+    })
+      .then(data => {
+        console.log(data);
+        return response.status(200).json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        return response.status(500).json({
+          message: err
+        });
+      });
+  });
+});
+
+exports.refreshToken = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    if (request.method !== "POST") {
+      return response.status(401).json({
+        message: "Not allowed"
+      });
+    }
+    rp({
+      method: "POST",
+      uri: "https://www.googleapis.com/oauth2/v4/token",
+      timeout: 30 * 1000,
+      json: {
+        client_id: functions.config().gapi.client_id,
+        client_secret: functions.config().gapi.client_secret,
+        refresh_token: request.body.refresh_token,
+        grant_type: "refresh_token"
+      }
+    })
+      .then(data => {
+        console.log(data);
+        return response.status(200).json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        return response.status(500).json({
+          message: err
+        });
+      });
+  });
+});
