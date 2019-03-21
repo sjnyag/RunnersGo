a360ba230d2a894a89e800f86308c815161d2cfa
sjnya <sjnyas@gmail.com>
2019-03-21 22:37:16 +0900

    Improve Login page

---
+++ src/components/Button.vue
@@ -0,0 +1,59 @@
+<template>
+  <button :class="color" @click="onClick()">{{name}}</button>
+</template>
+<script>
+export default {
+  name: 'Button',
+  props: { name: { default: '' }, color: { default: 'green' } },
+  methods: {
+    onClick() {
+      this.$emit('onclick')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$color: #5a7ab3;
+$shadowColor: darken($color, 10%);
+$activeColor: darken($color, 8%);
+button {
+  &.green {
+    $color: #00e676;
+    $shadowColor: darken($color, 10%);
+    $activeColor: darken($color, 8%);
+    background-color: $color;
+    box-shadow: 0 6px $shadowColor;
+    &:hover {
+      box-shadow: 0 5px $shadowColor;
+    }
+    &:active {
+      background-color: $activeColor;
+    }
+  }
+  background-color: $color;
+  box-shadow: 0 6px $shadowColor;
+  &:hover {
+    box-shadow: 0 5px $shadowColor;
+    top: 1px;
+  }
+  &:active {
+    background-color: $activeColor;
+    box-shadow: 0 0 $shadowColor;
+    top: 6px;
+  }
+  &:focus {
+    outline: none;
+  }
+  padding: 0.5em 2em;
+  border: none;
+  display: inline-block;
+  font-size: 114%;
+  line-height: 1;
+  color: #fff;
+  position: relative;
+  border-radius: 0.5em;
+  top: 0;
+  transition: 0.2s box-shadow, 0.2s top, 0.4s background;
+  cursor: pointer;
+}
+</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    <button id="login" @click="login">Login</button>
+  <div class="base mdc-layout-grid">
+    <div class="mdc-layout-grid__inner">
+      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 title-row">
+        <h1 class="title">Runners GO</h1>
+      </div>
+      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 logo-row">
+        <img :src="require('../assets/images/logo.png')">
+      </div>
+      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+        <flat-button id="login" @onclick="login" name="ログイン" color="green"></flat-button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { mapActions } from 'vuex'
+import FlatButton from './Button'
 export default {
   name: 'Login',
+  components: {
+    FlatButton
+  },
   methods: {
     login() {
       this.signIn().then(() => {
@@ -17,3 +31,60 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+$blur: 2px;
+div.base {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('../assets/images/background.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: #464646;
+  &:before {
+    content: '';
+    -webkit-filter: blur($blur);
+    -moz-filter: blur($blur);
+    -o-filter: blur($blur);
+    -ms-filter: blur($blur);
+    filter: blur($blur);
+    position: absolute;
+    top: -$blur;
+    left: -$blur;
+    right: -$blur;
+    bottom: -$blur;
+    z-index: -1;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+@import '@material/layout-grid/mdc-layout-grid.scss';
+div.mdc-layout-grid__cell {
+  justify-self: center;
+  align-self: center;
+  text-align: center;
+}
+div.title-row {
+  // width: 100vw;
+  // background-color: rgba(0, 0, 0, 0.5);
+  h1.title {
+    color: white;
+    font-weight: bold;
+    margin: 10px;
+    font-size: 300%;
+    text-shadow: 5px 5px 12px rgba(41, 182, 246, 0.3);
+  }
+}
+div.logo-row {
+  img {
+    margin: 20px;
+    width: auto;
+    height: 20vh;
+  }
+}
+button {
+  margin: 20px;
+}
+</style>
