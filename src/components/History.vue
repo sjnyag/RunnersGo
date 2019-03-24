f3fbd4dcd01edcddebf82f9d204356ef1cab5ad1
sjnya <sjnyas@gmail.com>
2019-03-24 16:22:47 +0900

    Create History page

---
+++ src/components/History.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <template v-for="(session, index) in sessions">
+      <session :key="'session_'+index" :session="session"></session>
+    </template>
+  </div>
+</template>
+<script>
+import Session from './Session'
+import moment from 'moment'
+
+export default {
+  name: 'History',
+  components: {
+    Session
+  },
+  data() {
+    return {
+      sessions: []
+    }
+  },
+  mounted() {
+    this.execApi()
+  },
+  methods: {
+    execApi() {
+      window.gapi.client.fitness.users.dataSources.datasets
+        .get({
+          userId: 'me',
+          dataSourceId: 'derived:com.google.active_minutes:com.google.android.gms:merge_active_minutes',
+          datasetId: this.lastWeek().unix() * 1000 * 1000 * 1000 + '-' + moment().unix() * 1000 * 1000 * 1000
+        })
+        .then(
+          response => {
+            const isContinuous = function(before, after) {
+              return (after - before) / (1000 * 1000 * 1000) < 15 * 60
+            }
+            const isWorkout = function(summary) {
+              return (summary.endTimeNanos - summary.startTimeNanos) / (1000 * 1000 * 1000) > 5 * 60
+            }
+            let before = 0
+            const result = []
+            response.result.point.forEach(point => {
+              if (isContinuous(before, point.startTimeNanos)) {
+                this._.last(result).summary.endTimeNanos = point.endTimeNanos
+              } else {
+                point.summary = {
+                  startTimeNanos: point.startTimeNanos,
+                  endTimeNanos: point.endTimeNanos
+                }
+                result.push(point)
+              }
+              before = point.endTimeNanos
+            })
+            this.sessions = this._.reverse(this._.filter(result, point => isWorkout(point.summary)))
+          },
+          reason => {
+            console.log('Error: ' + reason.result.error.message)
+          }
+        )
+    },
+    lastWeek: function() {
+      return moment(new Date()).subtract(7, 'days')
+    }
+  }
+}
+</script>
